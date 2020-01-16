068cc227834316e1aa5f4586f44512ff0d8525a1
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-01-15 20:27:48 -0500

    begin devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root to: "application#home"
+
+devise_for :users, :controllers => {registrations: 'registrations'}
 end
